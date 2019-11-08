db83f7a28cb8f28e46201463d5c277369798b502
liufeng <49270939+fengfengisme@users.noreply.github.com>
2019-11-08 09:04:41 +0800

    Create stack_learn.cpp

---
+++ stack_learn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    stack<int> s;
+    /*
+    s.push(item);		//将item压入栈顶
+    s.pop();			//删除栈顶的元素，但不会返回
+    s.top();			//返回栈顶的元素，但不会删除
+    s.size();			//返回栈中元素的个数
+    s.empty();			//检查栈是否为空，如果为空返回true，否则返回false
+    */
+    return 0;
+}
